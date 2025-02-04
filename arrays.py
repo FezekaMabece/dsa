5b9a731ea04bde66dfe40b0cb5a718a1ad661d7a
FezekaMabece <fmabece@gmail.com>
2025-02-04 14:03:07 +0200

    Linked Lists

--- arrays.py
+++ arrays.py
@@ -1,5 +1,23 @@
 new_list = [1,2,3,4,5,6,7,8,9,10]
-result = new_list[1]
+result = new_list[0]
 
+#linear search
 if 1 in new_list:
     print(True)
+
+
+#Append
+class_reg=["Paul","John","Peter","James","Jude"]
+new_student = class_reg.append("Sello")
+print(class_reg)
+
+#Insert
+class_reg.insert(2,"Thuli")
+print(class_reg)
+
+#Extend
+class_reg.extend(["Jabu","Thabo"])
+print(class_reg)
+
+
+ 
