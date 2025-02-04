c32b39226fefc2b24d8d757e474a328575dc91b2
FezekaMabece <fmabece@gmail.com>
2025-02-04 12:26:57 +0200

    Learning DSA

---
+++ arrays.py
@@ -0,0 +1,5 @@
+new_list = [1,2,3,4,5,6,7,8,9,10]
+result = new_list[1]
+
+if 1 in new_list:
+    print(True)
---
+++ recursive_binary_search.py
@@ -0,0 +1,29 @@
+from linear_search import result
+
+
+def recursive_binary_search(list,target):
+    if len(list) == 0:
+        return False
+    else:
+        midpoint = (len(list)) // 2
+
+        if list[midpoint] == target:
+            return True
+        else:
+            if list[midpoint] < target:
+                return recursive_binary_search(list[midpoint+1:], target)
+            else:
+                return recursive_binary_search(list[:midpoint], target)
+
+
+def verify(result):
+    print("Target found: ", result)
+
+numbers=[1,2,3,4,5,6,7,8,9,10]
+empty = []
+result = recursive_binary_search(numbers, 3)
+verify(result)
+
+result = recursive_binary_search(empty, 12)
+verify(result)
+
